71caa9581fd229d623bcf8e2b784739f4bfc445e
Sourcery AI <>
2022-05-06 16:34:16 +0000

    'Refactored by Sourcery'

--- demo/python/client.py
+++ demo/python/client.py
@@ -7,7 +7,7 @@ host = "localhost"
 port = 7000
 
 async def ws_test():
-    socketURL = "ws://"+host+":"+str(port)+"/"
+    socketURL = f"ws://{host}:{str(port)}/"
     print("Attempting websocket connection: ", socketURL)
     async with websockets.connect(socketURL) as websocket:
         data = await websocket.recv()
